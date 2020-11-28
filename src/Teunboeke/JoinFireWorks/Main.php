aa7ec043170f660b86a43974102df97a24a6bab5
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-28 19:33:24 +0100

    Update Main.php

--- src/Teunboeke/JoinFireWorks/Main.php
+++ src/Teunboeke/JoinFireWorks/Main.php
@@ -4,3 +4,12 @@ namespace Teunboeke\JoinFireWorks;
 
 use Teunboeke\Fireworks\item\Fireworks;
 use Teunboeke\Fireworks\Entity\FireworksRocket;
+
+use pocketmine\Player;
+use pocketmine\entity\Entity;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\math\Vector3;
+use pocketmine\plugin\PluginBase;
