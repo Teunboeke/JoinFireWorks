18737f3a0dd67e3560f03c9c79d32c0c5c1653aa
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-28 19:47:04 +0100

    Update Fireworks.php

--- src/Teunboeke/Fireworks/Item/Fireworks.php
+++ src/Teunboeke/Fireworks/Item/Fireworks.php
@@ -5,3 +5,11 @@ declare(strict_types = 1);
 namespace Teunboeke\Fireworks\Item
   
 use pocketmine\block\Block;
+use pocketmine\entity\Entity;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\Player;
+
+class Fireworks extends Item {
