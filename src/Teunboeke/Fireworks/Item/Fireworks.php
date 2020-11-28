ca8fceb2cb5c80f0e04dcc4c502ab02b7d3d6cee
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-28 19:51:38 +0100

    Update Fireworks.php

--- src/Teunboeke/Fireworks/Item/Fireworks.php
+++ src/Teunboeke/Fireworks/Item/Fireworks.php
@@ -29,3 +29,11 @@ class Fireworks extends Item {
   	public const COLOR_DARK_PURPLE = "\x05";
   	public const COLOR_DARK_AQUA = "\x06";
   	public const COLOR_GRAY = "\x07";
+  	public const COLOR_DARK_GRAY = "\x08";
+  	public const COLOR_PINK = "\x09";
+  	public const COLOR_GREEN = "\x0a";
+  	public const COLOR_YELLOW = "\x0b";
+  	public const COLOR_LIGHT_AQUA = "\x0c";
+  	public const COLOR_DARK_PINK = "\x0d";
+  	public const COLOR_GOLD = "\x0e";
+  	public const COLOR_WHITE = "\x0f";
