725c6846fa200aaae43964f78025be0d45c6a3f9
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-28 19:07:47 +0100

    Update FireworksRocket.php

--- src/Teunboeke/Fireworks/Entity/FireworksRocket.php
+++ src/Teunboeke/Fireworks/Entity/FireworksRocket.php
@@ -11,4 +11,11 @@ use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\ActorEventPacket;
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 
-class FireworksRocket extends entity{
+class FireworksRocket extends Entity {
+  
+  	public const NETWORK_ID = Entity::FIREWORKS_ROCKET;
+
+  	public const DATA_FIREWORK_ITEM = 16; //firework item
+  
+  	public $width = 0.25;
+  	public $height = 0.25;
