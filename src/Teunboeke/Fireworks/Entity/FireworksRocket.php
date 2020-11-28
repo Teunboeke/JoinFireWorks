8ea8eb057e4542263e56b86c48ef4c4e8a7b3ef1
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-28 19:06:45 +0100

    Update FireworksRocket.php

--- src/Teunboeke/Fireworks/Entity/FireworksRocket.php
+++ src/Teunboeke/Fireworks/Entity/FireworksRocket.php
@@ -3,3 +3,12 @@
 declare(strict_types=1);
 
 namespace Teunboeke\FireWorks\entity;
+
+use BlockHorizons\Fireworks\item\Fireworks;
+use pocketmine\entity\Entity;
+use pocketmine\level\Level;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\protocol\ActorEventPacket;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+
+class FireworksRocket extends entity{
