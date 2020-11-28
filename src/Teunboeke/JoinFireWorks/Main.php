59133c5f39e6b23f1cadf96d4f3532701d8333b1
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-28 19:35:15 +0100

    Update Main.php

--- src/Teunboeke/JoinFireWorks/Main.php
+++ src/Teunboeke/JoinFireWorks/Main.php
@@ -13,3 +13,14 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+  	public function onEnable() {
+      		ItemFactory::registerItem(new Fireworks());
+      		Item::initCreativeItems();
+      		if(!Entity::registerEntity(FireworksRocket::class, false, ["FireworksRocket"])) {
+            			$this->getLogger()->error("Failed to register FireworksRocket entity with savename 'FireworksRocket'");
+          }
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }   
