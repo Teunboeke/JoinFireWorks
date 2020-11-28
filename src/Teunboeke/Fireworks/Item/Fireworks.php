2fd0ae86ba9a417804b75179991abb1b7d28b67a
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-28 19:48:28 +0100

    Update Fireworks.php

--- src/Teunboeke/Fireworks/Item/Fireworks.php
+++ src/Teunboeke/Fireworks/Item/Fireworks.php
@@ -13,3 +13,12 @@ use pocketmine\nbt\tag\ListTag;
 use pocketmine\Player;
 
 class Fireworks extends Item {
+
+  	public const TYPE_SMALL_SPHERE = 0;
+  	public const TYPE_HUGE_SPHERE = 1;
+  	public const TYPE_STAR = 2;
+  	public const TYPE_CREEPER_HEAD = 3;
+  	public const TYPE_BURST = 4;
+  
+  	//color = chr(dye metadata)
+  	public const COLOR_BLACK = "\x00";
